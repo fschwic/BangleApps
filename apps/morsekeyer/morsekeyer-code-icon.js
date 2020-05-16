2e7eabd3796a67a8c583fb7d4cbc7845ee01772d
Frank Schwichtenberg <Frank@Schwichtenberg.net>
2020-05-16 10:25:13 +0200

    own icon

--- apps/morsekeyer/morsekeyer-code-icon.js
+++ apps/morsekeyer/morsekeyer-code-icon.js
@@ -1,1 +1,1 @@
-require("heatshrink").decompress(atob("mEwwkCIf4A/AH4A/AH4A/AA8RAAgHBgIHEiAXIp3uAAdQgEeA4ngC/4XoqlEAAaHBigHEoAQBgtVAAK4Thw2CC/4X/C8cBiIABC6YA/AH4A/AH4A/AHw"))
+require("heatshrink").decompress(atob("mEwhAZWldWJNov/F/4v/F/4v/F/4v/F/4v/F/4v/F/4v/F/4vzlcrF6ITBAA4uQwPX6+BF54TCAA4bJEIwUDQpAvFCYgAHUJwvvPYesR7QbJAA9WIJTvHCYIAHFyCdND7wv/F/4v/F/4v/F/4v/F/4v/F/4v/F/4v/F/4v4A"))
