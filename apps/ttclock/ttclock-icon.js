c26c021ab868e4eee8dc334acbd0fa77c3815d53
Frank Schwichtenberg <Frank@Schwichtenberg.net>
2020-10-08 12:52:17 +0200

    ttclock: icons

---
+++ apps/ttclock/ttclock-icon.js
@@ -0,0 +1,1 @@
+require("heatshrink").decompress(atob("mEwgIhegUYxAFCg8W9vAAoUf3+ACIUalodIh/Ag/h/AFD8YFB6EggE2CIPQkeAAp4XEyEgg/3EYJDCF4QRCmEMDoYFPC4mf8EH+P8LIwAS8EQAoe//4FD339UYoDCkg7BpAFBv1whl8AoN6n/8AoV+n/sAoUAKAIAInXAjwFCl/nj5eBAoWfAoQAiA=="))
